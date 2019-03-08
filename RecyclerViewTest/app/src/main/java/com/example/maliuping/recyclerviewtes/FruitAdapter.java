15afb612389691822fab7118ebd3d6943a4f13dd
maliuping <maliuping.270@foxmail.com>
2019-03-08 18:06:42 +0800

    add RecyclerViewTest

---
+++ RecyclerViewTest/app/src/main/java/com/example/maliuping/recyclerviewtes/FruitAdapter.java
@@ -0,0 +1,36 @@
+package com.example.maliuping.recyclerviewtes;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+
+    private List<Fruit> mFruitList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
+
+        public  ViewHolder (View view) {
+            super(view);
+            fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        }
+    }
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        mFruitList = fruitList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewHolder parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+}
